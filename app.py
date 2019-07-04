fc95323f7f4726797d7b2cc19422b98c4c98df44
Rajiv2806 <rajiv2806@gmail.com>
2019-07-04 17:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+print("*"*50)
 print(os.environ['APP_SETTINGS'])
-
+print("*"*50)
 
 @app.route('/')
 def hello():
